29bfa28e49f4c09582ed03d9f22a27e10d0f6dce
coord.e <me@coord-e.com>
2017-03-23 22:59:24 +0900

    Trying to make language switcher dropdown (not working yet)

--- js/navigation.js
+++ js/navigation.js
@@ -30,3 +30,16 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function() {
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+    $menu.children("li").click(function() {
+      $menu.removeClass("show-menu");
+      $button.html($(this).html());
+    });
+  });
+});
