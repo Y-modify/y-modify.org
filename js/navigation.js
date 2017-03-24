2d1b6cc6003c606ff7dd67dfb85d59215cb547c1
coord.e <me@coord-e.com>
2017-03-24 22:52:51 +0900

    Language switching link worked. Changed the link's class from "*--tools" to "*--link" Removed dropdown's code

--- js/navigation.js
+++ js/navigation.js
@@ -30,16 +30,3 @@ $(document).ready(function() {
     });
   });
 });
-
-$(document).ready(function() {
-  $(".dropdown-button").click(function() {
-    var $button, $menu;
-    $button = $(this);
-    $menu = $button.siblings(".dropdown-menu");
-    $menu.toggleClass("show-menu");
-    $menu.children("li").click(function() {
-      $menu.removeClass("show-menu");
-      $button.html($(this).html());
-    });
-  });
-});
