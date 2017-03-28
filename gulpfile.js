77632191fd353b81b8aef48cf26be7bb51e715a8
coord.e <me@coord-e.com>
2017-03-28 20:16:34 +0900

    Use 'imagemin' in 'process' task

--- gulpfile.js
+++ gulpfile.js
@@ -165,7 +165,7 @@ gulp.task('imagecopy', function() {
   .pipe(gulp.dest('dest/images'));
 });
 
-gulp.task('process', ['fonts', 'imagecopy', 'css', 'js', 'ejs', 'alllang']);
+gulp.task('process', ['fonts', 'imagemin', 'css', 'js', 'ejs', 'alllang']);
 
 gulp.task('watch' ,['browser-sync'] ,function(){
     gulp.watch('sass/**/*.scss', ['css']);
